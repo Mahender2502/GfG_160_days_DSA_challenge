93b3f5079fb311adf192febf27a20d155a5d56ee
Mahender2502 <vangarimahender2@gmail.com>
2025-04-04 12:55:23 -0700

    Added Move_All_zeroes_to_End.cpp

---
+++ OneDrive/Desktop/Gfg_160_days/Move_All_Zeroes_to_End.cpp
@@ -0,0 +1,59 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+
+// User function template for C++
+class Solution
+{
+public:
+    void pushZerosToEnd(vector<int> &arr)
+    {
+        int index = 0;
+        for (int i = 0; i < arr.size(); i++)
+        {
+            if (arr[i] != 0)
+            {
+                arr[index] = arr[i];
+                index += 1;
+            }
+        }
+        while (index < arr.size())
+        {
+            arr[index] = 0;
+            index += 1;
+        }
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--)
+    {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number)
+        {
+            arr.push_back(number);
+        }
+        Solution ob;
+        int n = arr.size();
+        ob.pushZerosToEnd(arr);
+        for (int i = 0; i < n; i++)
+        {
+            cout << arr[i] << " ";
+        }
+        cout << "\n";
+        cout << "~" << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
